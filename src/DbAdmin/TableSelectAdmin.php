056b9b36741280450f35b3f90566e1aa77eab432
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-09-19 04:01:29 +0200

    Fix.

--- src/DbAdmin/TableSelectAdmin.php
+++ src/DbAdmin/TableSelectAdmin.php
@@ -465,8 +465,12 @@ class TableSelectAdmin extends AbstractAdmin
             ];
             foreach ($unique_array as $key => $val) {
                 $key = \trim($key);
-                $type = isset($fields[$key]) ? $fields[$key]->type : '';
-                $collation = $fields[$key]->collation;
+                $type = '';
+                $collation = '';
+                if (isset($fields[$key])) {
+                    $type = $fields[$key]->type;
+                    $collation = $fields[$key]->collation;
+                }
                 if (($this->driver->jush() == "sql" || $this->driver->jush() == "pgsql") &&
                     \preg_match('~char|text|enum|set~', $type) && strlen($val) > 64) {
                     $key = (\strpos($key, '(') ? $key : $this->driver->escapeId($key)); //! columns looking like functions
