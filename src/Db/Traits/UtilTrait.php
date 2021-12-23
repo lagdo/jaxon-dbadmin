1a0bc13fad9d2fda18126939e8f7cba966be2c21
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-12-23 19:54:10 +0100

    Function refactoring.

---
+++ src/Db/Traits/UtilTrait.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Lagdo\DbAdmin\Db\Traits;
+
+use Lagdo\DbAdmin\Driver\Entity\TableFieldEntity;
+
+trait UtilTrait
+{
+    /**
+     * Escape for HTML
+     *
+     * @param string|null $string
+     *
+     * @return string
+     */
+    abstract public function html($string): string;
+
+    /**
+     * Get escaped error message
+     *
+     * @return string
+     */
+    public function error(): string
+    {
+        return $this->html($this->driver->error());
+    }
+
+    /**
+     * Check if the string is e-mail address
+     *
+     * @param mixed $email
+     *
+     * @return bool
+     */
+    public function isMail($email): bool
+    {
+        if (!\is_string($email)) {
+            return false;
+        }
+        $atom = '[-a-z0-9!#$%&\'*+/=?^_`{|}~]'; // characters of local-name
+        $domain = '[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])'; // one domain component
+        $pattern = "$atom+(\\.$atom+)*@($domain?\\.)+$domain";
+        return \preg_match("(^$pattern(,\\s*$pattern)*\$)i", $email) > 0;
+    }
+
+    /**
+     * Check if the string is URL address
+     *
+     * @param mixed $string
+     *
+     * @return bool
+     */
+    public function isUrl($string): bool
+    {
+        $domain = '[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])'; // one domain component //! IDN
+        //! restrict path, query and fragment characters
+        return \preg_match("~^(https?)://($domain?\\.)+$domain(:\\d+)?(/.*)?(\\?.*)?(#.*)?\$~i", $string) > 0;
+    }
+
+    /**
+     * Check if field should be shortened
+     *
+     * @param TableFieldEntity $field
+     *
+     * @return bool
+     */
+    public function isShortable(TableFieldEntity $field): bool
+    {
+        return \preg_match('~char|text|json|lob|geometry|point|linestring|polygon|string|bytea~', $field->type) > 0;
+    }
+
+    /**
+     * Get INI boolean value
+     *
+     * @param string $ini
+     *
+     * @return bool
+     */
+    public function iniBool(string $ini): bool
+    {
+        $value = \ini_get($ini);
+        return (\preg_match('~^(on|true|yes)$~i', $value) || (int) $value); // boolean values set by php_value are strings
+    }
+
+    /**
+     * Get INI bytes value
+     *
+     * @param string
+     *
+     * @return int
+     */
+    public function iniBytes(string $ini): int
+    {
+        $value = \ini_get($ini);
+        $unit = \strtolower(\substr($value, -1)); // Get the last char
+        $ival = \intval(\substr($value, 0, -1)); // Remove the last char
+        switch ($unit) {
+            case 'g': $value = $ival * 1024 * 1024 * 1024; break;
+            case 'm': $value = $ival * 1024 * 1024; break;
+            case 'k': $value = $ival * 1024; break;
+        }
+        return \intval($value);
+    }
+
+    /**
+     * Escape column key used in where()
+     *
+     * @param string
+     *
+     * @return string
+     */
+    public function escapeKey(string $key): string
+    {
+        if (\preg_match('(^([\w(]+)(' .
+            \str_replace('_', '.*', \preg_quote($this->driver->escapeId('_'))) . ')([ \w)]+)$)', $key, $match)) {
+            //! columns looking like functions
+            return $match[1] . $this->driver->escapeId($this->driver->unescapeId($match[2])) . $match[3]; //! SQL injection
+        }
+        return $this->driver->escapeId($key);
+    }
+
+    /**
+     * Create repeat pattern for preg
+     *
+     * @param string $pattern
+     * @param int $length
+     *
+     * @return string
+     */
+    public function repeatPattern(string $pattern, int $length)
+    {
+        // fix for Compilation failed: number too big in {} quantifier
+        // can create {0,0} which is OK
+        return \str_repeat("$pattern{0,65535}", $length / 65535) . "$pattern{0," . ($length % 65535) . '}';
+    }
+
+    /**
+     * Shorten UTF-8 string
+     *
+     * @param string $string
+     * @param int $length
+     * @param string $suffix
+     *
+     * @return string
+     */
+    public function shortenUtf8(string $string, int $length = 80, string $suffix = '')
+    {
+        if (!\preg_match('(^(' . $this->repeatPattern("[\t\r\n -\x{10FFFF}]", $length) . ')($)?)u', $string, $match)) {
+            // ~s causes trash in $match[2] under some PHP versions, (.|\n) is slow
+            \preg_match('(^(' . $this->repeatPattern("[\t\r\n -~]", $length) . ')($)?)', $string, $match);
+        }
+        return $this->html($match[1]) . $suffix . (isset($match[2]) ? '' : '<i>…</i>');
+    }
+
+    /**
+     * Escape or unescape string to use inside form []
+     *
+     * @param string $idf
+     * @param bool $back
+     *
+     * @return string
+     */
+    public function bracketEscape(string $idf, bool $back = false)
+    {
+        // escape brackets inside name='x[]'
+        static $trans = [':' => ':1', ']' => ':2', '[' => ':3', '"' => ':4'];
+        return \strtr($idf, ($back ? \array_flip($trans) : $trans));
+    }
+}
--- src/Db/Util.php
+++ src/Db/Util.php
@@ -13,6 +13,9 @@ use function intval;
 class Util implements UtilInterface
 {
     use UtilTrait;
+    use Traits\UtilTrait;
+    use Traits\SelectQueryTrait;
+    use Traits\DumpUtilTrait;
 
     /**
      * The constructor
@@ -42,186 +45,6 @@ class Util implements UtilInterface
     public function blankTarget(): string
     {
         return ' target="_blank" rel="noreferrer noopener"';
-    }
-
-    /**
-     * Get escaped error message
-     *
-     * @return string
-     */
-    public function error(): string
-    {
-        return $this->html($this->driver->error());
-    }
-
-    /**
-     * Check if the string is e-mail address
-     *
-     * @param mixed $email
-     *
-     * @return bool
-     */
-    public function isMail($email): bool
-    {
-        if (!\is_string($email)) {
-            return false;
-        }
-        $atom = '[-a-z0-9!#$%&\'*+/=?^_`{|}~]'; // characters of local-name
-        $domain = '[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])'; // one domain component
-        $pattern = "$atom+(\\.$atom+)*@($domain?\\.)+$domain";
-        return \preg_match("(^$pattern(,\\s*$pattern)*\$)i", $email) > 0;
-    }
-
-    /**
-     * Check if the string is URL address
-     *
-     * @param mixed $string
-     *
-     * @return bool
-     */
-    public function isUrl($string): bool
-    {
-        $domain = '[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])'; // one domain component //! IDN
-        //! restrict path, query and fragment characters
-        return \preg_match("~^(https?)://($domain?\\.)+$domain(:\\d+)?(/.*)?(\\?.*)?(#.*)?\$~i", $string) > 0;
-    }
-
-    /**
-     * Check if field should be shortened
-     *
-     * @param TableFieldEntity $field
-     *
-     * @return bool
-     */
-    public function isShortable(TableFieldEntity $field): bool
-    {
-        return \preg_match('~char|text|json|lob|geometry|point|linestring|polygon|string|bytea~', $field->type) > 0;
-    }
-
-    /**
-     * Get INI boolean value
-     *
-     * @param string $ini
-     *
-     * @return bool
-     */
-    public function iniBool(string $ini): bool
-    {
-        $value = \ini_get($ini);
-        return (\preg_match('~^(on|true|yes)$~i', $value) || (int) $value); // boolean values set by php_value are strings
-    }
-
-    /**
-     * Get INI bytes value
-     *
-     * @param string
-     *
-     * @return int
-     */
-    public function iniBytes(string $ini): int
-    {
-        $value = \ini_get($ini);
-        $unit = \strtolower(\substr($value, -1)); // Get the last char
-        $ival = \intval(\substr($value, 0, -1)); // Remove the last char
-        switch ($unit) {
-            case 'g': $value = $ival * 1024 * 1024 * 1024; break;
-            case 'm': $value = $ival * 1024 * 1024; break;
-            case 'k': $value = $ival * 1024; break;
-        }
-        return \intval($value);
-    }
-
-    /**
-     * Escape column key used in where()
-     *
-     * @param string
-     *
-     * @return string
-     */
-    public function escapeKey(string $key): string
-    {
-        if (\preg_match('(^([\w(]+)(' .
-            \str_replace('_', '.*', \preg_quote($this->driver->escapeId('_'))) . ')([ \w)]+)$)', $key, $match)) {
-            //! columns looking like functions
-            return $match[1] . $this->driver->escapeId($this->driver->unescapeId($match[2])) . $match[3]; //! SQL injection
-        }
-        return $this->driver->escapeId($key);
-    }
-
-    /**
-     * Compute fields() from input edit data
-     *
-     * @return array
-     */
-    public function getFieldsFromEdit()
-    {
-        $fields = [];
-        $values = $this->input->values;
-        foreach ((array) $values['field_keys'] as $key => $value) {
-            if ($value != '') {
-                $value = $this->bracketEscape($value);
-                $values['function'][$value] = $values['field_funs'][$key];
-                $values['fields'][$value] = $values['field_vals'][$key];
-            }
-        }
-        foreach ((array) $values['fields'] as $key => $value) {
-            $name = $this->bracketEscape($key, 1); // 1 - back
-            $fields[$name] = [
-                'name' => $name,
-                'privileges' => ['insert' => 1, 'update' => 1],
-                'null' => 1,
-                'autoIncrement' => false, // ($key == $this->driver->primaryIdName()),
-            ];
-        }
-        return $fields;
-    }
-
-    /**
-     * Create repeat pattern for preg
-     *
-     * @param string $pattern
-     * @param int $length
-     *
-     * @return string
-     */
-    public function repeatPattern(string $pattern, int $length)
-    {
-        // fix for Compilation failed: number too big in {} quantifier
-        // can create {0,0} which is OK
-        return \str_repeat("$pattern{0,65535}", $length / 65535) . "$pattern{0," . ($length % 65535) . '}';
-    }
-
-    /**
-     * Shorten UTF-8 string
-     *
-     * @param string $string
-     * @param int $length
-     * @param string $suffix
-     *
-     * @return string
-     */
-    public function shortenUtf8(string $string, int $length = 80, string $suffix = '')
-    {
-        if (!\preg_match('(^(' . $this->repeatPattern("[\t\r\n -\x{10FFFF}]", $length) . ')($)?)u', $string, $match)) {
-            // ~s causes trash in $match[2] under some PHP versions, (.|\n) is slow
-            \preg_match('(^(' . $this->repeatPattern("[\t\r\n -~]", $length) . ')($)?)', $string, $match);
-        }
-        return $this->html($match[1]) . $suffix . (isset($match[2]) ? '' : '<i>…</i>');
-    }
-
-    /**
-     * Escape or unescape string to use inside form []
-     *
-     * @param string $idf
-     * @param bool $back
-     *
-     * @return string
-     */
-    public function bracketEscape(string $idf, bool $back = false)
-    {
-        // escape brackets inside name='x[]'
-        static $trans = [':' => ':1', ']' => ':2', '[' => ':3', '"' => ':4'];
-        return \strtr($idf, ($back ? \array_flip($trans) : $trans));
     }
 
     /**
@@ -275,71 +98,13 @@ class Util implements UtilInterface
     }
 
     /**
-     * Returns export format options
-     *
-     * @return array
-     */
-    public function dumpFormat()
-    {
-        return ['sql' => 'SQL', 'csv' => 'CSV,', 'csv;' => 'CSV;', 'tsv' => 'TSV'];
-    }
-
-    /**
-     * Returns export output options
-     *
-     * @return array
-     */
-    public function dumpOutput()
-    {
-        $output = ['text' => $this->trans->lang('open'), 'file' => $this->trans->lang('save')];
-        if (\function_exists('gzencode')) {
-            $output['gz'] = 'gzip';
-        }
-        return $output;
-    }
-
-    /**
-     * Set the path of the file for webserver load
-     *
-     * @return string
-     */
-    public function importServerPath()
-    {
-        return 'adminer.sql';
-    }
-
-    /**
-     * Export database structure
-     *
-     * @param string
-     *
-     * @return null prints data
-     */
-    // public function dumpDatabase($database) {
-    // }
-
-    /**
-     * Print before edit form
-     *
-     * @param string $table
-     * @param array $fields
-     * @param mixed $row
-     * @param bool $update
-     *
-     * @return null
-     */
-    // public function editRowPrint(string $table, array $fields, $row, bool $update)
-    // {
-    // }
-
-    /**
      * Functions displayed in edit form
      *
      * @param TableFieldEntity $field Single field from fields()
      *
      * @return array
      */
-    public function editFunctions(TableFieldEntity $field)
+    public function editFunctions(TableFieldEntity $field): array
     {
         $update = isset($this->input->values['select']); // || $this->where([]);
         if ($field->autoIncrement && !$update) {
@@ -406,66 +171,36 @@ class Util implements UtilInterface
     }
 
     /**
-     * Filter length value including enums
+     * Process sent input
      *
-     * @param string $length
+     * @param TableFieldEntity $field Single field from fields()
+     * @param string $value
+     * @param string $function
      *
      * @return string
      */
-    public function processLength(string $length)
+    private function _processInput(TableFieldEntity $field, string $value, string $function = '')
     {
-        if (!$length) {
-            return '';
+        if ($function == 'SQL') {
+            return $value; // SQL injection
         }
-        $enumLength = $this->driver->enumLength();
-        return (\preg_match("~^\\s*\\(?\\s*$enumLength(?:\\s*,\\s*$enumLength)*+\\s*\\)?\\s*\$~", $length) &&
-            \preg_match_all("~$enumLength~", $length, $matches) ? '(' . \implode(',', $matches[0]) . ')' :
-            \preg_replace('~^[0-9].*~', '(\0)', \preg_replace('~[^-0-9,+()[\]]~', '', $length))
-        );
-    }
-
-    /**
-     * Create SQL string from field type
-     *
-     * @param TableFieldEntity $field
-     * @param string $collate
-     *
-     * @return string
-     */
-    private function processType(TableFieldEntity $field, string $collate = 'COLLATE')
-    {
-        $values = [
-            'unsigned' => $field->unsigned,
-            'collation' => $field->collation,
-        ];
-        return ' ' . $field->type . $this->processLength($field->length) .
-            (\preg_match($this->driver->numberRegex(), $field->type) &&
-            \in_array($values['unsigned'], $this->driver->unsigned()) ?
-            " {$values['unsigned']}" : '') . (\preg_match('~char|text|enum|set~', $field->type) &&
-            $values['collation'] ? " $collate " . $this->driver->quote($values['collation']) : '');
-    }
-
-    /**
-     * Create SQL string from field
-     *
-     * @param TableFieldEntity $field Basic field information
-     * @param TableFieldEntity $typeField Information about field type
-     *
-     * @return array
-     */
-    public function processField(TableFieldEntity $field, TableFieldEntity $typeField)
-    {
-        return [
-            $this->driver->escapeId(trim($field->name)),
-            $this->processType($typeField),
-            ($field->null ? ' NULL' : ' NOT NULL'), // NULL for timestamp
-            $this->driver->defaultValue($field),
-            (\preg_match('~timestamp|datetime~', $field->type) && $field->onUpdate ?
-                " ON UPDATE {$field->onUpdate}" : ''),
-            ($this->driver->support('comment') && $field->comment != '' ?
-                ' COMMENT ' . $this->driver->quote($field->comment) : ''),
-            ($field->autoIncrement ? $this->driver->autoIncrement() : null),
-        ];
+        $name = $field->name;
+        $expression = $this->driver->quote($value);
+        if (\preg_match('~^(now|getdate|uuid)$~', $function)) {
+            $expression = "$function()";
+        } elseif (\preg_match('~^current_(date|timestamp)$~', $function)) {
+            $expression = $function;
+        } elseif (\preg_match('~^([+-]|\|\|)$~', $function)) {
+            $expression = $this->driver->escapeId($name) . " $function $expression";
+        } elseif (\preg_match('~^[+-] interval$~', $function)) {
+            $expression = $this->driver->escapeId($name) . " $function " .
+                (\preg_match("~^(\\d+|'[0-9.: -]') [A-Z_]+\$~i", $value) ? $value : $expression);
+        } elseif (\preg_match('~^(addtime|subtime|concat)$~', $function)) {
+            $expression = "$function(" . $this->driver->escapeId($name) . ", $expression)";
+        } elseif (\preg_match('~^(md5|sha1|password|encrypt)$~', $function)) {
+            $expression = "$function($expression)";
+        }
+        return $this->driver->unconvertField($field, $expression);
     }
 
     /**
@@ -519,196 +254,6 @@ class Util implements UtilInterface
         }
         return $this->_processInput($field, $value, $function);
     }
-
-    /**
-     * Apply SQL function
-     *
-     * @param string $function
-     * @param string $column escaped column identifier
-     *
-     * @return string
-     */
-    public function applySqlFunction(string $function, string $column)
-    {
-        return ($function ? ($function == 'unixepoch' ?
-            "DATETIME($column, '$function')" : ($function == 'count distinct' ?
-            'COUNT(DISTINCT ' : strtoupper("$function(")) . "$column)") : $column);
-    }
-
-    /**
-     * Process columns box in select
-     *
-     * @return array
-     */
-    public function processSelectColumns()
-    {
-        $select = []; // select expressions, empty for *
-        $group = []; // expressions without aggregation - will be used for GROUP BY if an aggregation function is used
-        foreach ((array) $this->input->values['columns'] as $key => $val) {
-            if ($val['fun'] == 'count' ||
-                ($val['col'] != '' && (!$val['fun'] ||
-                \in_array($val['fun'], $this->driver->functions()) ||
-                \in_array($val['fun'], $this->driver->grouping())))) {
-                $select[$key] = $this->applySqlFunction(
-                    $val['fun'],
-                    ($val['col'] != '' ? $this->driver->escapeId($val['col']) : '*')
-                );
-                if (!in_array($val['fun'], $this->driver->grouping())) {
-                    $group[] = $select[$key];
-                }
-            }
-        }
-        return [$select, $group];
-    }
-
-    /**
-     * Process sent input
-     *
-     * @param TableFieldEntity $field Single field from fields()
-     * @param string $value
-     * @param string $function
-     *
-     * @return string
-     */
-    private function _processInput(TableFieldEntity $field, string $value, string $function = '')
-    {
-        if ($function == 'SQL') {
-            return $value; // SQL injection
-        }
-        $name = $field->name;
-        $expression = $this->driver->quote($value);
-        if (\preg_match('~^(now|getdate|uuid)$~', $function)) {
-            $expression = "$function()";
-        } elseif (\preg_match('~^current_(date|timestamp)$~', $function)) {
-            $expression = $function;
-        } elseif (\preg_match('~^([+-]|\|\|)$~', $function)) {
-            $expression = $this->driver->escapeId($name) . " $function $expression";
-        } elseif (\preg_match('~^[+-] interval$~', $function)) {
-            $expression = $this->driver->escapeId($name) . " $function " .
-                (\preg_match("~^(\\d+|'[0-9.: -]') [A-Z_]+\$~i", $value) ? $value : $expression);
-        } elseif (\preg_match('~^(addtime|subtime|concat)$~', $function)) {
-            $expression = "$function(" . $this->driver->escapeId($name) . ", $expression)";
-        } elseif (\preg_match('~^(md5|sha1|password|encrypt)$~', $function)) {
-            $expression = "$function($expression)";
-        }
-        return $this->driver->unconvertField($field, $expression);
-    }
-
-    /**
-     * Process search box in select
-     *
-     * @param array $fields
-     * @param array $indexes
-     *
-     * @return array
-     */
-    public function processSelectSearch(array $fields, array $indexes)
-    {
-        $expressions = [];
-        foreach ($indexes as $i => $index) {
-            if ($index->type == 'FULLTEXT' && $this->input->values['fulltext'][$i] != '') {
-                $columns = \array_map(function ($column) {
-                    return $this->driver->escapeId($column);
-                }, $index->columns);
-                $expressions[] = 'MATCH (' . \implode(', ', $columns) . ') AGAINST (' .
-                    $this->driver->quote($this->input->values['fulltext'][$i]) .
-                    (isset($this->input->values['boolean'][$i]) ? ' IN BOOLEAN MODE' : '') . ')';
-            }
-        }
-        foreach ((array) $this->input->values['where'] as $key => $val) {
-            if ("$val[col]$val[val]" != '' && in_array($val['op'], $this->driver->operators())) {
-                $prefix = '';
-                $cond = " $val[op]";
-                if (\preg_match('~IN$~', $val['op'])) {
-                    $in = $this->processLength($val['val']);
-                    $cond .= ' ' . ($in != '' ? $in : '(NULL)');
-                } elseif ($val['op'] == 'SQL') {
-                    $cond = " $val[val]"; // SQL injection
-                } elseif ($val['op'] == 'LIKE %%') {
-                    $cond = ' LIKE ' . $this->_processInput($fields[$val['col']], "%$val[val]%");
-                } elseif ($val['op'] == 'ILIKE %%') {
-                    $cond = ' ILIKE ' . $this->_processInput($fields[$val['col']], "%$val[val]%");
-                } elseif ($val['op'] == 'FIND_IN_SET') {
-                    $prefix = "$val[op](" . $this->driver->quote($val['val']) . ', ';
-                    $cond = ')';
-                } elseif (!\preg_match('~NULL$~', $val['op'])) {
-                    $cond .= ' ' . $this->_processInput($fields[$val['col']], $val['val']);
-                }
-                if ($val['col'] != '') {
-                    $expressions[] = $prefix . $this->driver->convertSearch(
-                        $this->driver->escapeId($val['col']),
-                        $val,
-                        $fields[$val['col']]
-                    ) . $cond;
-                } else {
-                    // find anywhere
-                    $cols = [];
-                    foreach ($fields as $name => $field) {
-                        if ((\preg_match('~^[-\d.' . (\preg_match('~IN$~', $val['op']) ? ',' : '') . ']+$~', $val['val']) ||
-                            !\preg_match('~' . $this->driver->numberRegex() . '|bit~', $field->type)) &&
-                            (!\preg_match("~[\x80-\xFF]~", $val['val']) || \preg_match('~char|text|enum|set~', $field->type)) &&
-                            (!\preg_match('~date|timestamp~', $field->type) || \preg_match('~^\d+-\d+-\d+~', $val['val']))
-                        ) {
-                            $cols[] = $prefix . $this->driver->convertSearch($this->driver->escapeId($name), $val, $field) . $cond;
-                        }
-                    }
-                    $expressions[] = ($cols ? '(' . \implode(' OR ', $cols) . ')' : '1 = 0');
-                }
-            }
-        }
-        return $expressions;
-    }
-
-    /**
-     * Process order box in select
-     *
-     * @return array
-     */
-    public function processSelectOrder(): array
-    {
-        $expressions = [];
-        foreach ((array) $this->input->values['order'] as $key => $val) {
-            if ($val != '') {
-                $regexp = '~^((COUNT\(DISTINCT |[A-Z0-9_]+\()(`(?:[^`]|``)+`|"(?:[^"]|"")+")\)|COUNT\(\*\))$~';
-                $expressions[] = (\preg_match($regexp, $val) ? $val : $this->driver->escapeId($val)) . //! MS SQL uses []
-                    (isset($this->input->values['desc'][$key]) ? ' DESC' : '');
-            }
-        }
-        return $expressions;
-    }
-
-    /**
-     * Process limit box in select
-     *
-     * @return int
-     */
-    public function processSelectLimit(): int
-    {
-        return (isset($this->input->values['limit']) ? intval($this->input->values['limit']) : 50);
-    }
-
-    /**
-     * Process length box in select
-     *
-     * @return int
-     */
-    public function processSelectLength(): int
-    {
-        return (isset($this->input->values['text_length']) ? intval($this->input->values['text_length']) : 100);
-    }
-
-    /**
-     * Process extras in select form
-     *
-     * @param array $where AND conditions
-     * @param array $foreignKeys
-     *
-     * @return bool
-     */
-    // public function processSelectEmail(array $where, array $foreignKeys)
-    // {
-    //     return false;
-    // }
 
     /**
      * Value printed in select table
