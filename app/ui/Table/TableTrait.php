693940f4435fd4cfdde49131f8d01cb7f90b2701
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-08-14 06:06:06 +0200

    Add a bottom margin on tab nav items.

--- app/ui/Table/TableTrait.php
+++ app/ui/Table/TableTrait.php
@@ -22,24 +22,19 @@ trait TableTrait
     {
         $firstTabId = array_key_first($tabs);
         return $this->ui->build(
-            $this->ui->row(
-                $this->ui->col(
-                    $this->ui->tabNav(
-                        $this->ui->each($tabs, fn($tab, $id) =>
-                            $this->ui->tabNavItem($this->ui->text($tab))
-                                ->target("tab-content-$id")
-                                ->active($firstTabId === $id)
-                        )
-                    ),
-                    $this->ui->tabContent(
-                        $this->ui->each($tabs, fn($_, $id) =>
-                            $this->ui->tabContentItem()
-                                ->setId("tab-content-$id")
-                                ->active($firstTabId === $id)
-                        )
-                    )
+            $this->ui->tabNav(
+                $this->ui->each($tabs, fn($tab, $id) =>
+                    $this->ui->tabNavItem($this->ui->text($tab))
+                        ->target("tab-content-$id")
+                        ->active($firstTabId === $id)
                 )
-                ->width(12)
+            )->setStyle('margin-bottom: 5px;'),
+            $this->ui->tabContent(
+                $this->ui->each($tabs, fn($_, $id) =>
+                    $this->ui->tabContentItem()
+                        ->setId("tab-content-$id")
+                        ->active($firstTabId === $id)
+                )
             )
         );
     }
