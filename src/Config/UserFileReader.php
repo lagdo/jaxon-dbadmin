83dce04f2196d89bcfcd40477ceb6cb234aa74ca
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-08-20 22:26:38 +0200

    Filter the servers list in the options on valid entries.

--- src/Config/UserFileReader.php
+++ src/Config/UserFileReader.php
@@ -5,7 +5,9 @@ namespace Lagdo\DbAdmin\Config;
 use Jaxon\Config\ConfigReader;
 use Jaxon\Config\ConfigSetter;
 
+use function array_map;
 use function array_filter;
+use function count;
 use function env;
 use function is_array;
 use function is_file;
@@ -24,6 +26,24 @@ class UserFileReader
     {}
 
     /**
+     * @param array $server
+     *
+     * @return bool
+     */
+    private function checkServer(array $server): bool
+    {
+        if (!isset($server['name']) || !isset($server['driver'])) {
+            return false;
+        }
+        if ($server['driver'] === 'sqlite') {
+            return isset($server['directory']) && count($server) === 3;
+        }
+        return isset($server['host']) && isset($server['port']) &&
+            isset($server['username']) && isset($server['password']) &&
+            count($server) === 6;
+    }
+
+    /**
      * Replace options with values from the .env config.
      *
      * @param array $values
@@ -32,16 +52,20 @@ class UserFileReader
      */
     private function getOptionValues(array $values): array
     {
+        // Filter the servers list on valid entries
+        $values['servers'] = array_filter($values['servers'],
+            fn(array $server) => $this->checkServer($server));
+
         if (!$this->useEnv) {
             return $values;
         }
+
         // The values in the server options are the names of the
         // corresponding options in the .env file.
-        $values['servers'] = array_map(function(array $server) {
-            foreach (['host', 'port', 'username', 'password'] as $option) {
-                if (isset($server[$option])) {
-                    $server[$option] = env($server[$option]);
-                }
+        $options = ['host', 'port', 'username', 'password'];
+        $values['servers'] = array_map(function(array $server) use($options) {
+            foreach ($options as $option) {
+                $server[$option] = env($server[$option]);
             }
             return $server;
         }, $values['servers'] ?? []);
@@ -57,19 +81,24 @@ class UserFileReader
      */
     public function getOptions(array $defaultOptions): array
     {
-        unset($defaultOptions['provider']); // Remove the provider field.
+        // If the config file doesn't exists, return an empty array.
+        if (!is_file($this->configFilePath)) {
+            return [];
+        }
+
+        // The key to use for the user options
+        $userKey = 'user';
+        // Remove the provider field.
+        unset($defaultOptions['provider']);
+
         $setter = new ConfigSetter();
         $reader = new ConfigReader($setter);
-        $userConfig = $setter->newConfig(['options' => $defaultOptions]);
-
-        if (!is_file($this->configFilePath)) {
-            return $userConfig->getOption('options');
-        }
+        $userConfig = $setter->newConfig([$userKey => $defaultOptions]);
 
         $config = $reader->load($setter->newConfig(), $this->configFilePath);
         $commonOptions = $config->getOption('common', null);
         if (is_array($commonOptions)) {
-            $userConfig = $setter->setOptions($userConfig, $commonOptions, 'options');
+            $userConfig = $setter->setOptions($userConfig, $commonOptions, $userKey);
         }
 
         $fallbackOptions = $config->getOption('fallback', null);
@@ -78,12 +107,12 @@ class UserFileReader
         $userOptions = $userOptions[0] ?? $fallbackOptions;
 
         if (!is_array($userOptions)) {
-            return $this->getOptionValues($userConfig->getOption('options'));
+            return $this->getOptionValues($userConfig->getOption($userKey));
         }
 
         unset($userOptions['id']); // Remove the id field.
-        $userConfig = $setter->setOptions($userConfig, $userOptions);
+        $userConfig = $setter->setOptions($userConfig, $userOptions, $userKey);
 
-        return $this->getOptionValues($userConfig->getOption('options'));
+        return $this->getOptionValues($userConfig->getOption($userKey));
     }
 }
