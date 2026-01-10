ad5828653d9a1a0551b5b4ef3b8e1568d266158d
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-01-10 14:21:21 +0100

    Comment.

--- src/UiData/Ddl/ColumnAction.php
+++ src/UiData/Ddl/ColumnAction.php
@@ -2,13 +2,13 @@
 
 namespace Lagdo\DbAdmin\Db\UiData\Ddl;
 
+/**
+ * Actions on a table column for create or alter operations.
+ */
 enum ColumnAction: string
 {
     case NONE = 'none';
-
     case ADD = 'add';
-
     case CHANGE = 'change';
-
     case DROP = 'drop';
 }
