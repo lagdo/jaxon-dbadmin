8259feb759fe7e92ac31b58803052ee462a09f8d
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-01-26 23:14:58 +0100

    Fix missing option setup.

--- app/ajax/Admin/Db/View/Dql/Select.php
+++ app/ajax/Admin/Db/View/Dql/Select.php
@@ -44,6 +44,7 @@ class Select extends MainComponent
         $selectData = $this->db()->getSelectData($table, $options);
         $this->setSelectBag('options', [
             'limit' => (int)($selectData->options['limit']['value'] ?? 0),
+            'total' => (bool)($options['total'] ?? true), // Keep the same value.
             'length' => (int)($selectData->options['length']['value'] ?? 0),
         ]);
         $this->stash()->set('select.query', $selectData->query);
